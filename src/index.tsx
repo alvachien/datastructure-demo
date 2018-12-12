e40229cfe0d5f336f581720b93b76b2bf0fe6962
Alva Chien <alvachien@163.com>
2018-12-12 09:51:16 +0800

    Using TypeScript in React instead of JavaScript

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+// import App from './App';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+import Hello from './components/hello';
+
+ReactDOM.render(
+  // <App />,
+  <Hello name="TypeScript" enthusiasmLevel={10} />,
+  document.getElementById('root') as HTMLElement
+);
+registerServiceWorker();
